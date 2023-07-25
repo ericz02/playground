a3d27ece1a3e607bd97a5fb4d57f9f7288b6f2f5
ericz02 <ezheng0720@gmail.com>
2023-07-24 22:37:05 -0400

    testing sidebar

---
+++ nextjs/src/app/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+
+  
+  return (
+    <div className="bg-blue-400 w-40 h-screen">
+      <div className="text-white p-2 hover:bg-blue-500 cursor-pointer">Communities</div>
+      <div className="text-white p-2 hover:bg-blue-500 cursor-pointer">Settings</div>
+      <div className="text-white p-2 hover:bg-blue-500 cursor-pointer">Contact Us</div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
 
         <Navbar />
-        
+        <Sidebar />
         {children}
       </body>
     </html>
